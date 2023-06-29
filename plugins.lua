8d89885701fd9b70bdbe031954baba72449249a0
HeziCyan <hezicyan@gmail.com>
2023-06-29 09:29:32 +0800

    chore: better source for cursor-word

--- plugins.lua
+++ plugins.lua
@@ -86,16 +86,19 @@ local plugins = {
   },
 
   {
-    "itchyny/vim-cursorword",
+    "yamatsum/nvim-cursorline",
     event = { "BufEnter", "BufNewFile" },
     config = function()
-      vim.api.nvim_command("augroup user_plugin_cursorword")
-      vim.api.nvim_command("autocmd!")
-      vim.api.nvim_command("autocmd FileType NvimTree,lspsagafinder,dashboard,vista let b:cursorword = 0")
-      vim.api.nvim_command("autocmd WinEnter * if &diff || &pvw | let b:cursorword = 0 | endif")
-      vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
-      vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
-      vim.api.nvim_command("augroup END")
+      require("nvim-cursorline").setup({
+        cursorline = {
+          enable = false,
+        },
+        cursorword = {
+          enable = true,
+          min_length = 1,
+          hl = { underline = true },
+        },
+      })
     end,
   },
 
